039256c6c92c3402397b7058bb3f1f78295135b9
WSH <251967795@qq.com>
2019-01-15 13:14:06 +0800

    ....

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/admin');
-Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/mobile');
+Yii::setAlias('@mobile', dirname(dirname(__DIR__)) . '/mobile');
+Yii::setAlias('@admin', dirname(dirname(__DIR__)) . '/admin');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
